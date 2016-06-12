7fe2e0aebd134e7260adb8a26628cdbcc9f1e74a
Thoralf Liersch <thoralf.liersch@gmail.com>
2016-06-12 16:48:06 +0200

    added CPU Clockspeed and CPU Voltage

--- check_raspberry.sh
+++ check_raspberry.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 case "$1" in
     temp)
-        # check core temperatur in celsius
         vcgencmd measure_temp | cut -d "=" -f 2 | cut -d "'" -f 1
         ;;
+    voltage)
+        vcgencmd measure_volts | cut -d "=" -f 2 | tr -d 'V'
+        ;;
+    clockspeed)
+        echo $(( $(vcgencmd measure_clock arm | cut -d "=" -f 2) / 1000))
+        ;;
     *)
-        echo "Usage: $N {temp}" >&2
+        echo "Usage: $N {temp|voltage|clockspeed}" >&2
 esac
 exit 0
